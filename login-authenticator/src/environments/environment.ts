ebb85aa929230a7648667b80b631639922f0f40c
vaibhavmishra571999 <vaibhav571999@gmail.com>
2020-08-23 13:12:19 +0530

    Developed Login Authenticator with Firebase Authentication Management

---
+++ login-authenticator/src/environments/environment.ts
@@ -0,0 +1,19 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyArOUCZBoC08bUgBeSXU-yOYpgSxdXeKrY",
+    authDomain: "angularlogin-31701.firebaseapp.com",
+    databaseURL: "https://angularlogin-31701.firebaseio.com",
+    projectId: "angularlogin-31701",
+    storageBucket: "angularlogin-31701.appspot.com",
+    messagingSenderId: "321578490070",
+    appId: "1:321578490070:web:e5324231d2e0292f6e5177",
+    measurementId: "G-10YLM5M7CP"
+
+  }
+};
